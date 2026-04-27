eb9202c7c3d98b2e4791d150b8bfa0061925114e
mastafapec00 <mastafapec00@gmail.com>
2026-04-27 18:10:57 +0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: 'green' }}>Pakistan Currency Exchange 🇵🇰</h1>
+      <p>Aap ki website ka setup kamyab ho gaya hai!</p>
+    </div>
+  );
+}
